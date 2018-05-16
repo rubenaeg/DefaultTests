a51d5c3f75808b680b2c6fbb654892593fb19f48
RubenA <ruben.aegerter@gmx.de>
2018-05-16 18:24:26 +0200

    add error handling for ECONNREFUSED

--- test/RoutingTest.js
+++ test/RoutingTest.js
@@ -1,77 +1,71 @@
 'use strict';
 
-const {app} = require('../app/app');
 const expect = require('chai').expect;
 const getPlatformRequestBuilder = require('jovo-framework').util.getPlatformRequestBuilder;
-const {send, addUserData, getUserData, removeUser, removeUserData} = require('jovo-framework').TestSuite;
-setDbPath(app.config.db.localDbFilename);
+const {send} = require('jovo-framework').TestSuite;
 
-describe('LAUNCH_INTENT', function() {
-    for(let requestBuilder of getPlatformRequestBuilder('GoogleActionDialogFlow', 'AlexaSkill')) {
-        it('should successfully go into LaunchIntent for ' + requestBuilder.type(), function (done) {
-            send(requestBuilder.launch())
-                .then((res) => {
-                    expect(res.isAsk('Hello World! What\'s your name?', 'Please tell me your name.')).to.equal(true);
-                    done();
-                });
-        });
-    }
-});
-
-describe('HELLO_WORLD_INTENT', function() {
-    for(let requestBuilder of getPlatformRequestBuilder('GoogleActionDialogFlow', 'AlexaSkill')) {
-        it('should successfully go into HelloWorldIntent for ' + requestBuilder.type(), function(done) {
-            send(requestBuilder.intent('HelloWorldIntent'))
-                .then((res) => {
-                    expect(res.isAsk('Hello World! What\'s your name?', 'Please tell me your name.')).to.equal(true);
-                    done();
-                })
-        });
-    }
-});
-
-describe('MY_NAME_IS_INTENT', function() {
-    for(let requestBuilder of getPlatformRequestBuilder('GoogleActionDialogFlow', 'AlexaSkill')) {
-        it('should simulate the whole conversation flow and greet the user with the correct name for ' + requestBuilder.type(), function(done) {
-            send(requestBuilder.launch())
-                .then(() => send(requestBuilder.intent('MyNameIsIntent', {name: 'John'})))
-                .then((res) => {
-                    expect(res.isTell('Hey John, nice to meet you!')).to.equal(true);
-                    done();
-                });
+for (let rb of getPlatformRequestBuilder('AlexaSkill', 'GoogleActionDialogFlow')) {
+    describe(rb.type(), function() {
+        describe('LAUNCH_INTENT', function () {
+            it('should successfully go into LaunchIntent for ' + rb.type(), function (done) {
+                send(rb.launch())
+                    .then((res) => {
+                        expect(res.isAsk('Hello World! What\'s your name?', 'Please tell me your name.')).to.equal(true);
+                        done();
+                    });
+            });
         });
 
-        it('should simulate deep invocation and directly go into MyNameIsIntent for ' + requestBuilder.type(), function(done) {
-            send(requestBuilder.intent().setIntentName('MyNameIsIntent').addInput('name', 'John'))
-                .then((res) => {
-                    expect(res.isTell('Hey John, nice to meet you!')).to.equal(true);
-                    done();
-                });
+        describe('HELLO_WORLD_INTENT', function () {
+            it('should successfully go into HelloWorldIntent for ' + rb.type(), function (done) {
+                send(rb.intent('HelloWorldIntent'))
+                    .then((res) => {
+                        expect(res.isAsk('Hello World! What\'s your name?', 'Please tell me your name.')).to.equal(true);
+                        done();
+                    })
+            });
         });
-    }
-});
 
-describe('HELP_INTENT', function() {
-    for(let requestBuilder of getPlatformRequestBuilder('GoogleActionDialogFlow', 'AlexaSkill')) {
-        it('should not throw an error, unless HelpIntent doesn\'t exist for ' + requestBuilder.type(), function(done) {
-            send(requestBuilder.intent('HelpIntent'))
-                .then((res) => {
-                    // expect HelpIntent
-                    done();
-                });
+        describe('MY_NAME_IS_INTENT', function () {
+            it('should simulate the whole conversation flow and greet the user with the correct name for ' + rb.type(), function (done) {
+                send(rb.launch())
+                    .then((res) => {
+                        expect(res.isAsk('Hello World! What\'s your name?', 'Please tell me your name.')).to.equal(true);
+                        return send(rb.intent('MyNameIsIntent', {name: 'John'}))
+                    })
+                    .then((res) => {
+                        expect(res.isTell('Hey John, nice to meet you!')).to.equal(true);
+                        done();
+                    });
+            });
+
+            it('should simulate deep invocation and directly go into MyNameIsIntent for ' + rb.type(), function (done) {
+                send(rb.intent().setIntentName('MyNameIsIntent').addInput('name', 'John'))
+                    .then((res) => {
+                        expect(res.isTell('Hey John, nice to meet you!')).to.equal(true);
+                        done();
+                    });
+            });
         });
-    }
-});
 
-describe('UNHANDLED', function() {
-    for(let requestBuilder of getPlatformRequestBuilder('GoogleActionDialogFlow', 'AlexaSkill')) {
-        it('should not throw an error, unless Unhandled is not defined for ' + requestBuilder.type(), function(done) {
-            send(requestBuilder.intent('Unhandled'))
-                .then((res) => {
-                    // expect HelpIntent
-                    done();
-                });
+        describe('HELP_INTENT', function () {
+            it('should not throw an error, unless HelpIntent doesn\'t exist for ' + rb.type(), function (done) {
+                send(rb.intent('HelpIntent'))
+                    .then((res) => {
+                        // expect HelpIntent
+                        done();
+                    });
+            });
         });
-    }
-});
 
+        describe('UNHANDLED', function () {
+            it('should not throw an error, unless Unhandled is not defined for ' + rb.type(), function (done) {
+                send(rb.intent('ShouldNotExistIntent'))
+                    .then((res) => {
+                        // expect Unhandled
+                        done();
+                    });
+            });
+        });
+    });
+}
